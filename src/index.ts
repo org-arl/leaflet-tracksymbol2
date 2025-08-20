3c445cc3c3f438d78f68efb83fe205d4029c7b54
Nick Ng <ngyewch@gmail.com>
2025-08-20 15:47:13 +0800

    feat: removed support for global L object

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-//@ts-strict-ignore
-import L, {type LatLngExpression} from 'leaflet';
+import {type LatLngExpression} from 'leaflet';
 
 import {TrackSymbol} from './trackSymbol.js';
 import {type ShapeSetEntry, type ShapeOptions, type TrackSymbolOptions} from './options.js';
@@ -28,15 +27,5 @@ export {
     PositionReport,
     ShipStaticData,
 };
-
-L['trackSymbol'] = function (latLng: LatLngExpression, options?: TrackSymbolOptions): TrackSymbol {
-    return new TrackSymbol(latLng, options);
-}
-L['TrackSymbol'] = TrackSymbol;
-
-L['aisTrackSymbol'] = function (positionReport: PositionReport, options?: AISTrackSymbolOptions): AISTrackSymbol {
-    return new AISTrackSymbol(positionReport, options);
-}
-L['AISTrackSymbol'] = AISTrackSymbol;
 
 export default TrackSymbol;
